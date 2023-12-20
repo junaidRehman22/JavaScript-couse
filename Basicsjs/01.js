53721ba3cdd2941a6ae8f3e93b5a0420a140aa26
junaidRehman22 <142348583+junaidRehman22@users.noreply.github.com>
2023-12-20 04:14:21 +0000

    variables

--- Basicsjs/01.js
+++ Basicsjs/01.js
@@ -1,1 +1,13 @@
-console.log("junaid rehman");
+// fullName = "Tony Stark";
+// age = 24;
+// price = 99.99;
+// isFollow = true;
+// isFollow = false;
+// a = null;
+// y = undefined;
+// console.log(age);
+// console.log(fullName);
+
+// var , let and const 
+let fullName = "tony stark";
+console.log(fullName);
