4ecffcf47ed40b2309e5607f70699f8e5fbbb965
junaidRehman22 <142348583+junaidRehman22@users.noreply.github.com>
2024-04-20 20:28:59 +0000

    conversion/operations

---
+++ Basicsjs/02.js
@@ -0,0 +1,17 @@
+// let score = "33abc";
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" ==> 33
+// "33abc" ==> NaN
+// true ==> 1 , false ===> 0
+//  undefined ==> NaN
+
+// let isLoggedIn = "11";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
