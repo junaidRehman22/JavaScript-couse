cf721edd16d9666ca47253575909052e47dfd9bb
junaidRehman22 <142348583+junaidRehman22@users.noreply.github.com>
2024-04-21 21:10:05 +0000

    operations

---
+++ Basicsjs/02conversionandoperation.js
@@ -0,0 +1,63 @@
+// let score = "33abc";
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" ==> 33
+// "33abc" ==> NaN
+// true ==> 1 , false ===> 0
+//  undefined ==> NaN
+
+// let isLoggedIn = "11";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// ############# OPERATIONS ###################
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+// let str1 = "hello"
+// let str2 = " junaid"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1 , num2 , num3
+// num1 = num2  = num3 = 2+2
+
+// Postfix and prefix operations 
+// let somevalue = 23
+// ++somevalue;
+// console.log(somevalue);
+
+
+//  comparison of datatypes
+// conditional statements(if-else for-loop and arrays) ko sikhne se pehle hamey comparison ke bare me pata hona chahiye
+
+// console.log(2 > 1);
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
