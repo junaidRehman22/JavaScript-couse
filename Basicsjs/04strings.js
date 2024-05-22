a9d7efe81caf208835442e5e3424d37bc7fd2c49
junaidRehman22 <142348583+junaidRehman22@users.noreply.github.com>
2024-05-22 10:42:28 +0000

    strings concatenation strings concatenation

---
+++ Basicsjs/04strings.js
@@ -0,0 +1,8 @@
+// using variables how we concatenate two or more "strings"
+const firstName = "junaid";
+const repoCount = 7;
+console.log(firstName + repoCount + "value");
+console.log(firstName + repoCount + " value");
+// This syntax i'am not recommended for anyone because this is outdated.In modern days no one is using this method .
+
+lg
