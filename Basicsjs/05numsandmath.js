5b80a67f36eba462b54a78d328cab8ac47af4888
junaidRehman22 <142348583+junaidRehman22@users.noreply.github.com>
2024-05-26 11:24:17 +0000

    random method

---
+++ Basicsjs/05numsandmath.js
@@ -0,0 +1,31 @@
+// const score = 400
+// console.log(score);
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// const otherNumber = 123.500
+// console.log(otherNumber.toPrecision(4));
+// const hundreds = 1000000
+// console.log(hundreds.tolocaleString());
+// ++++++++++++++++++++++++++++ Math +++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-7));
+// console.log(Math.round(4.7700));
+// console.log(Math.ceil(4.972));
+// console.log(Math.floor(4.912));
+// console.log(Math.min(4,3,6,9));
+// console.log(Math.max(4,3,6,10,20));
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math.random())
+// //  its value comes in between 0 and 1.
+// console.log(Math.floor((Math.random() *10) + 1))
+
+
+// const min = 10
+// const max = 20
+
+// console.log(Math.floor(Math.random() *(max - min + 1) + min) )
